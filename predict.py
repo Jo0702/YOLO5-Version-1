f84fffb2325e296ba41adaec298778370a76555c
chadoc0101 <101116745+chadoc0101@users.noreply.github.com>
2022-04-23 08:08:12 +0000

    Update predict.py

--- predict.py
+++ predict.py
@@ -4,8 +4,8 @@
 #-----------------------------------------------------------------------#
 import time
 import streamlit as st
-import cv2
-import numpy as np
+# import cv2
+# import numpy as np
 from PIL import Image
 
 from yolo import YOLO
@@ -86,48 +86,48 @@ if __name__ == "__main__":
         else:
             st.title(":balloon:You have not selected a picture :")
             st.caption("buluuuuuuuuuuuu")
-    elif mode == "video":
-        capture = cv2.VideoCapture(video_path)
-        if video_save_path!="":
-            fourcc  = cv2.VideoWriter_fourcc(*'XVID')
-            size    = (int(capture.get(cv2.CAP_PROP_FRAME_WIDTH)), int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-            out     = cv2.VideoWriter(video_save_path, fourcc, video_fps, size)
+#     elif mode == "video":
+#         capture = cv2.VideoCapture(video_path)
+#         if video_save_path!="":
+#             fourcc  = cv2.VideoWriter_fourcc(*'XVID')
+#             size    = (int(capture.get(cv2.CAP_PROP_FRAME_WIDTH)), int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+#             out     = cv2.VideoWriter(video_save_path, fourcc, video_fps, size)
 
-        fps = 0.0
-        while(True):
-            t1 = time.time()
-            # 读取某一帧
-            ref, frame = capture.read()
-            if not ref:
-                break
-            # 格式转变，BGRtoRGB
-            frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-            # 转变成Image
-            frame = Image.fromarray(np.uint8(frame))
-            # 进行检测
-            frame = np.array(yolo.detect_image(frame))
-            # RGBtoBGR满足opencv显示格式
-            frame = cv2.cvtColor(frame,cv2.COLOR_RGB2BGR)
+#         fps = 0.0
+#         while(True):
+#             t1 = time.time()
+#             # 读取某一帧
+#             ref, frame = capture.read()
+#             if not ref:
+#                 break
+#             # 格式转变，BGRtoRGB
+#             frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+#             # 转变成Image
+#             frame = Image.fromarray(np.uint8(frame))
+#             # 进行检测
+#             frame = np.array(yolo.detect_image(frame))
+#             # RGBtoBGR满足opencv显示格式
+#             frame = cv2.cvtColor(frame,cv2.COLOR_RGB2BGR)
             
-            fps  = ( fps + (1./(time.time()-t1)) ) / 2
-            print("fps= %.2f"%(fps))
-            frame = cv2.putText(frame, "fps= %.2f"%(fps), (0, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+#             fps  = ( fps + (1./(time.time()-t1)) ) / 2
+#             print("fps= %.2f"%(fps))
+#             frame = cv2.putText(frame, "fps= %.2f"%(fps), (0, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
             
-            cv2.imshow("video",frame)
-            c= cv2.waitKey(1) & 0xff 
-            if video_save_path!="":
-                out.write(frame)
+#             cv2.imshow("video",frame)
+#             c= cv2.waitKey(1) & 0xff 
+#             if video_save_path!="":
+#                 out.write(frame)
 
-            if c==27:
-                capture.release()
-                break
+#             if c==27:
+#                 capture.release()
+#                 break
 
-        print("Video Detection Done!")
-        capture.release()
-        if video_save_path!="":
-            print("Save processed video to the path :" + video_save_path)
-            out.release()
-        cv2.destroyAllWindows()
+#         print("Video Detection Done!")
+#         capture.release()
+#         if video_save_path!="":
+#             print("Save processed video to the path :" + video_save_path)
+#             out.release()
+#         cv2.destroyAllWindows()
 
     # elif mode == "fps":
     #     img = Image.open('img/street.jpg')
